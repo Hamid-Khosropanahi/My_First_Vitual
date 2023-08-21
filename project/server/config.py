5790a581bbbe16f3cfbf1ee5db321b316a311622
Hamid_ROG <Hamid.khp@hotmail.com>
2023-08-21 22:03:31 +0330

    First_Code

--- project/server/config.py
+++ project/server/config.py
@@ -12,14 +12,14 @@ class BaseConfig:
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
     SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
-
+    
 
 class TestingConfig(BaseConfig):
     """Testing configuration."""
@@ -28,10 +28,11 @@ class TestingConfig(BaseConfig):
     BCRYPT_LOG_ROUNDS = 4
     SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
-
+    
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
     SECRET_KEY = 'my_precious'
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
+    
